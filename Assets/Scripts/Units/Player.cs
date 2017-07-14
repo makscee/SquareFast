74585f4dff3ebc5ded9a3b6bf82bf46950a010e9
makscee <shadeoflance@gmail.com>
2017-07-14 12:33:21 +0300

    Add delay text

--- Assets/Scripts/Units/Player.cs
+++ Assets/Scripts/Units/Player.cs
@@ -1,25 +1,40 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Runtime.InteropServices;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Player : Unit
 {
 	public static Player Instance;
+	public Text DelayText;
 
 	private void Awake()
 	{
 		Instance = this;
 	}
 
+	private float dt = 0;
 	private void Update()
 	{
+		dt += Time.deltaTime;
 		bool leftDown = Input.GetButtonDown("Left"),
 			rightDown = Input.GetButtonDown("Right");
 		var dir = leftDown ? -1 : (rightDown ? 1 : 0);
 		if (dir == 0) return;
+		DelayText.text = Math.Round(dt / Level.TickTime / 3, 3).ToString();
 		MoveOrAttack(dir);
+	}
+
+	public override bool TickUpdate()
+	{
+		if (Level.Ticks % 3 == 0)
+		{
+			dt = 0;
+		}
+		return base.TickUpdate();
 	}
 
 	public override void Die()
