49dd616ca7e927f682cce6e6663075cfbcb6030b
makscee <shadeoflance@gmail.com>
2017-10-09 00:15:39 +0300

    TimeText scale animation

--- Assets/Scripts/CounterScript.cs
+++ Assets/Scripts/CounterScript.cs
@@ -26,7 +26,16 @@ public class CounterScript : MonoBehaviour
             _started = false;
             return;
         }
+        var d = Level.TickTime * 3;
+        var t = Math.Ceiling(_t / d);
         _t += Time.deltaTime;
+        if (t < Math.Ceiling(_t / d))
+        {
+            Utils.Animate(new Vector3(1.3f, 1.3f), Vector3.one, 0.2f, (v) =>
+            {
+                _text.transform.localScale = v;
+            }, null, true);
+        }
         _text.text = string.Format("{0:F2}", Math.Round(_t, 2));
     }
 }
